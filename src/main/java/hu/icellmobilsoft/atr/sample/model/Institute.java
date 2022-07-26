d81a62ea05429a639fc4d7bbdc7b7a0fa23344cf
Juhasz Katalin Ilona <juhasz.katalin.ilona@gmail.com>
2022-07-26 14:44:30 +0200

    added institute model

---
+++ src/main/java/hu/icellmobilsoft/atr/sample/model/Institute.java
@@ -0,0 +1,39 @@
+package hu.icellmobilsoft.atr.sample.model;
+
+import java.util.ArrayList;
+
+public class Institute {
+
+    private String id;
+    private String name;
+    private ArrayList<Department> departments = new ArrayList<Department>();
+
+    public Institute() {
+    }
+
+    public Institute(String id, String name, ArrayList<Department> dep) {
+        this.id = id.trim();
+        this.name = name.trim();
+        this.departments = dep;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void addDepartments(Department department) {
+        this.departments.add(department);
+    }
+
+    public Institute getInstance() {
+        return this;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
