98a7758b363f95838e31201b316333aad3d6dfc6
Juhasz Katalin Ilona <juhasz.katalin.ilona@gmail.com>
2022-07-26 14:18:22 +0200

    rm comments

--- src/main/java/hu/icellmobilsoft/atr/sample/model/Department.java
+++ src/main/java/hu/icellmobilsoft/atr/sample/model/Department.java
@@ -1,13 +1,9 @@
 package hu.icellmobilsoft.atr.sample.model;
 
-import java.util.ArrayList;
-
 public class Department {
 
-    // private Department departments[];
     private String id;
     private String name;
-    // ArrayList<Department> departmentList = new ArrayList<Department>();
 
     public String getId() {
         return id;
@@ -28,27 +24,4 @@ public class Department {
     public Department getInstance() {
         return this;
     }
-
-    // public void AddNewDepartment(String id, String name) {
-    //     departmentList.add(new Department(id, name));
-    // }
-
-    // public String getDepartmentById(String id) {
-    //     for (int index = 0; index < departmentList.size(); index++) {
-    //         if (departmentList.get(index).id.equals(id)) {
-    //             return departmentList.get(index).name;
-    //         }
-    //     }
-    //     return "";
-    // }
-
-    // public ArrayList<Department> getDepartmenList() {
-    //     return departmentList;
-    // }
-
-    // public static void main(String[] args) {
-    // Department oDep = new Department();
-    // oDep.AddNewDepartment("1", "buci");
-    // System.out.println(oDep.getDepartmenList());
-    // }
 }
