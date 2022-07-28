a7eff22816287258a3ee4756aa7f3a0096d2450a
Juhasz Katalin Ilona <juhasz.katalin.ilona@gmail.com>
2022-07-28 16:23:34 +0200

    added patient and institute to jsonParser

--- src/main/java/hu/icellmobilsoft/atr/sample/action/SamplePatientAction.java
+++ src/main/java/hu/icellmobilsoft/atr/sample/action/SamplePatientAction.java
@@ -1,28 +1,19 @@
 package hu.icellmobilsoft.atr.sample.action;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-
-// import org.json.simple.*;
-
-// import org.json.simple.parser.*;
-// import org.json.*;
-// import org.json.JSONArray;
-// import org.json.JSONException;
-// import org.json.JSONObject;
-// import org.json.JSONString;
-import org.json.simple.parser.JSONParser;
-
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 import hu.icellmobilsoft.atr.sample.parseXml;
 import hu.icellmobilsoft.atr.sample.model.Department;
+import hu.icellmobilsoft.atr.sample.model.Institute;
 import hu.icellmobilsoft.atr.sample.model.Patient;
 import hu.icellmobilsoft.atr.sample.repository.DepartmentRepository;
 import hu.icellmobilsoft.atr.sample.repository.InstituteRepository;
@@ -58,7 +49,7 @@ public class SamplePatientAction {
     }
 
     public DepartmentRepository getDepartments(JSONObject jsonObject) {
-        DepartmentRepository tempDepRepFor = new DepartmentRepository();
+        DepartmentRepository tempDepRep = new DepartmentRepository();
 
         JSONObject sample = (JSONObject) jsonObject.get("sample");
         JSONObject departments = (JSONObject) sample.get("departments");
@@ -67,19 +58,79 @@ public class SamplePatientAction {
         for (int i = 0; i < department.size(); i++) {
             String id = ((JSONObject) department.get(i)).get("id").toString();
             String name = ((JSONObject) department.get(i)).get("name").toString();
-            tempDepRepFor.saveDepartment(new Department(id, name));
+            tempDepRep.saveDepartment(new Department(id, name));
         }
 
-        System.out.println(depRep.toString());
-        return tempDepRepFor;
+        tempDepRep.toString();
+        return tempDepRep;
+    }
+
+    public InstituteRepository getInstitute(JSONObject jsonObject) {
+        InstituteRepository tempInstRep = new InstituteRepository();
+
+        JSONObject sample = (JSONObject) jsonObject.get("sample");
+        JSONObject institutes = (JSONObject) sample.get("institutes");
+        JSONArray institute = (JSONArray) institutes.get("institute");
+
+        for (int i = 0; i < institute.size(); i++) {
+            Institute tempInst = new Institute();
+            tempInst.setId(
+                    ((JSONObject) institute.get(i)).get("id").toString());
+            tempInst.setName(
+                    ((JSONObject) institute.get(i)).get("name").toString());
+            JSONObject departments = (JSONObject) ((JSONObject) institute.get(i)).get("departments");
+            if (departments.get("department") instanceof java.util.ArrayList) {
+                JSONArray department = (JSONArray) departments.get("department");
+                for (int j = 0; j < department.size(); j++) {
+                    String depId = department.get(j).toString();
+                    tempInst.addDepartments(new Department(depId, this.depRep.findDepartment(depId).getName()));
+                }
+            } else {
+                String depId = departments.get("department").toString();
+                tempInst.addDepartments(new Department(depId, this.depRep.findDepartment(depId).getName()));
+            }
+
+            tempInstRep.saveInstitute(tempInst);
+        }
+
+        tempInstRep.toString();
+        return tempInstRep;
+    }
+
+    public PatientRepository getPatients(JSONObject jsonObject) {
+        PatientRepository tempPatRep = new PatientRepository();
+
+        JSONObject sample = (JSONObject) jsonObject.get("sample");
+        JSONObject patients = (JSONObject) sample.get("patients");
+        JSONArray patient = (JSONArray) patients.get("patient");
+
+        for (int i = 0; i < patient.size(); i++) {
+            Patient tempPatient = new Patient();
+
+            String id = ((JSONObject) patient.get(i)).get("id").toString();
+            String name = ((JSONObject) patient.get(i)).get("name").toString();
+            String email = ((JSONObject) patient.get(i)).get("email").toString();
+            String username = ((JSONObject) patient.get(i)).get("username").toString();
+            String department = ((JSONObject) patient.get(i)).get("department").toString();
+            String institute = ((JSONObject) patient.get(i)).get("institute").toString();
+
+            tempPatient.setId(id);
+            tempPatient.setName(name);
+            tempPatient.setEmail(email);
+            tempPatient.setUsername(username);
+            tempPatient.setDepartment(this.depRep.findDepartment(department));
+            tempPatient.setInstitute(this.instRep.findInstitute(institute));
+            tempPatRep.savePatient(tempPatient);
+        }
+
+        tempPatRep.toString();
+        return tempPatRep;
     }
 
     public void loadFromJson(String fileName) {
         JSONParser parser = new JSONParser();
-        DepartmentRepository depRepForJson = new DepartmentRepository();
 
         try {
-            // Object obj = parser.parse(new FileReader("/Users/User/Desktop/course.json"));
             InputStream in = this.getClass().getClassLoader().getResourceAsStream(fileName);
             String text = new BufferedReader(
                     new InputStreamReader(in, StandardCharsets.UTF_8))
@@ -88,8 +139,9 @@ public class SamplePatientAction {
             Object obj = parser.parse(text);
 
             JSONObject jsonObject = (JSONObject) obj;
-            depRepForJson = getDepartments(jsonObject);
-            // System.out.println(jsonObject);
+            this.depRep = getDepartments(jsonObject);
+            this.instRep = getInstitute(jsonObject);
+            this.patRep = getPatients(jsonObject);
 
         } catch (Exception e) {
             e.printStackTrace();
